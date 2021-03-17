3c0ef343d7d9d2b2089851a60c2c7c7906440dd8
mace286 <christian.kruse@atip.de>
2021-03-17 10:39:36 +0100

    init

---
+++ main/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Greeting();
+            Console.ReadLine();
+        }
+
+        static void Greeting()
+        {
+            Console.WriteLine("hi user");
+        }
+    }
+}
